d324b4993d88734d012c075a455bb15d6bbcd34c
priscilla-lee <plee3@wellesley.edu>
2015-04-23 21:25:56 -0400

    priscilla made changes

--- CS249EventsApp/CS249EventsApp.js
+++ CS249EventsApp/CS249EventsApp.js
@@ -1,6 +1,8 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  
+  console.log("HELLO. PRISCILLA WAS HERE");
 
   Template.hello.helpers({
     counter: function () {
